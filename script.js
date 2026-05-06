bebe05d0dfb8c2cb6b8ade23e3e3db583ab4b079
Codercosmos12 <ayanshayan507@gmail.com>
2026-05-06 10:53:46 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-const lenis = new lenis({
-    duration: 1.2,
-    smoothWheel: true,
-    easing: (t) => 1 - Math.pow(1 - t, 3),
-});
 
-function raf(time){
-    lenis.raf(time);
-    requestAnimationFrame(raf);
-};
-
-requestAnimationFrame(raf)
 
 
  const dot = document.querySelector(".cursor-dot")
